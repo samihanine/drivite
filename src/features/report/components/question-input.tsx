6ab063558743800b421c225cd1735cf019448461
samihanine <sami.hanine22@gmail.com>
2024-12-05 01:45:50 -0500

    feat: Add QuestionInput component for dynamic question handling; support various input types and update answer functionality

---
+++ src/features/report/components/question-input.tsx
@@ -0,0 +1,161 @@
+import { Input } from "@/components/input";
+import { Label } from "@/components/label";
+import { RadioGroup, RadioGroupItem } from "@/components/radio-group";
+import { Slider } from "@/components/slider";
+import { InsertAnswer, Question } from "@/db";
+
+export default function QuestionInput({
+  question,
+  value,
+  updateAnswer,
+}: {
+  question: Question;
+  updateAnswer: (answer: { questionId: string; value: string }) => void;
+  value: string;
+}) {
+  return (
+    <>
+      {" "}
+      <Label>
+        {question.label}
+
+        {question.required && <span className="text-red-500 ml-1">*</span>}
+      </Label>
+      {question.type === "TEXT" && (
+        <Input
+          type="text"
+          required={question.required || false}
+          value={value}
+          onChange={(e) => {
+            updateAnswer({
+              questionId: question.id,
+              value: e.target.value,
+            });
+          }}
+        />
+      )}
+      {question.type === "NUMBER" && (
+        <Input
+          type="number"
+          required={question.required || false}
+          value={value}
+          onChange={(e) => {
+            updateAnswer({
+              questionId: question.id,
+              value: e.target.value,
+            });
+          }}
+        />
+      )}
+      {question.type === "PERCENTAGE" && (
+        <div className="flex items-center gap-5">
+          <Slider
+            className="bg-gray-100"
+            value={[parseInt(value || "0")]}
+            onValueChange={(value) => {
+              updateAnswer({
+                questionId: question.id,
+                value: value[0].toString(),
+              });
+            }}
+            step={10}
+            min={0}
+            max={100}
+          />
+
+          <div className="flex items-center gap-3">
+            <Input
+              required={question.required || false}
+              type="number"
+              value={value}
+              onChange={(e) => {
+                updateAnswer({
+                  questionId: question.id,
+                  value: e.target.value,
+                });
+              }}
+            />
+            %
+          </div>
+        </div>
+      )}
+      {question.type === "SELECT" && (
+        <RadioGroup
+          className="flex gap-5 flex-row"
+          required={question.required || false}
+          onValueChange={(value) => {
+            updateAnswer({
+              questionId: question.id,
+              value: value,
+            });
+          }}
+          name={question.id}
+          value={value}
+        >
+          {question.options.map((option) => (
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value={option} id={question.id + option} />
+              <Label className="cursor-pointer" htmlFor={question.id + option}>
+                {option}
+              </Label>
+            </div>
+          ))}
+        </RadioGroup>
+      )}
+      {question.type === "IMAGE" && (
+        <div className="flex flex-col gap-2">
+          {!!value?.length && (
+            <img src={value} className="h-40 w-full object-contain" />
+          )}
+
+          <Input
+            type="file"
+            accept="image/*"
+            required={question.required || false}
+            onChange={async (e) => {
+              const file = e.target.files?.[0];
+              if (!file) return;
+              const imageValue = URL.createObjectURL(file);
+              updateAnswer({
+                questionId: question.id,
+                value: imageValue,
+              });
+            }}
+          />
+        </div>
+      )}
+      {question.type === "DATE" && (
+        <Input
+          type="date"
+          value={
+            value
+              ? new Date(value || "").toISOString().split("T")[0]
+              : undefined
+          }
+          required={question.required || false}
+          onChange={(e) => {
+            updateAnswer({
+              questionId: question.id,
+              value: e.target.value,
+            });
+          }}
+        />
+      )}
+      {question.type === "DATETIME" && (
+        <Input
+          type="datetime-local"
+          value={
+            value ? new Date(value || "").toISOString().slice(0, 16) : undefined
+          }
+          required={question.required || false}
+          onChange={(e) => {
+            updateAnswer({
+              questionId: question.id,
+              value: e.target.value,
+            });
+          }}
+        />
+      )}
+    </>
+  );
+}
